2f62fee6eb50d74eb1e1a24bc25b4e3ea5bef0bd
IUnknown* pUnk <73540952+IUnknown-pUnk-Sec-lab@users.noreply.github.com>
2025-10-19 19:02:11 +0100

    Enhance export display with entry points

--- GetExports.py
+++ GetExports.py
@@ -10,8 +10,18 @@ if not hasattr(pe, 'DIRECTORY_ENTRY_EXPORT'):
     print("No export table found.")
     sys.exit(0)
 
-print(f"Exports for: {sys.argv[1]}\n")
+print(f"Exports and EntryPoints for: {sys.argv[1]}\n")
 print(f"{'Ordinal':>7}  {'RVA':>10}  Name")
 for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
     name = exp.name.decode() if exp.name else b'<none>'
     print(f"{exp.ordinal:7}  0x{exp.address:08X}  {name}")
+
+print(f" \n")
+
+print("EntryPoint RVA: 0x%X" % pe.OPTIONAL_HEADER.AddressOfEntryPoint)
+print(f" \n")
+for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
+    name = exp.name.decode() if exp.name else "<none>"
+    print(hex(exp.address), name)
+    
+print(f" \n")
